b787e04c68e971b78169b0d51f06ac78c42aecb3
Gui14 <guilhermepimentelgui@gmail.com>
2023-10-20 11:08:56 -0300

    update

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3xzxhxhxb42#qccmgzorbd$7*dp%e1gnz&xp!4)otoj@(661l2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

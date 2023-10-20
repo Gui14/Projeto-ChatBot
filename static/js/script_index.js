05b4553c58f3656ab0658798631deaa323a404d8
Gui14 <guilhermepimentelgui@gmail.com>
2023-10-20 09:51:18 -0300

    update

---
+++ static/js/script_index.js
@@ -0,0 +1,7 @@
+document.getElementById("botoes-acao").addEventListener("click", function() {
+    document.getElementById("popupContainer").style.display = "block";
+});
+
+document.getElementById("closePopup").addEventListener("click", function() {
+    document.getElementById("popupContainer").style.display = "none";
+});
